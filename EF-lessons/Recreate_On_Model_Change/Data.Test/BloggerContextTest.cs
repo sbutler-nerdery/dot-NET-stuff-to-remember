a6e7d9ed30246c5a396a78bed74e65b8ede2bd1d
Stan Butler <sbutler@nerdery.com>
2013-04-26 11:39:06 -0500

    Refactored this example to use a unit test instead of a console application.

---
+++ EF-lessons/Recreate_On_Model_Change/Data.Test/BloggerContextTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using Data.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Data.Test
+{
+    [TestClass]
+    public class BloggerContextTest
+    {
+        [TestMethod]
+        public void CreateDatabase()
+        {
+            //This will tell EF to drop and recreate the database whenever the models change...
+            Database.SetInitializer(new BloggerContextInitializer());
+
+            using (var db = new BloggerContext())
+            {
+                //Get the seed values from the database and print them to the screen
+                Blog seedBlog = db.Blogs.FirstOrDefault();
+                Assert.AreNotEqual(seedBlog, null);
+                Assert.AreEqual(seedBlog.Name, "My Blog");
+                //Assert.AreEqual(seedBlog.Url, "www.myblog.com");
+
+                //Assert that the post titles are not empty... just for the heck of it... 
+                seedBlog.Posts.ForEach(post => Assert.AreNotEqual(post.Title, string.Empty));
+            }
+        }
+    }
+}
--- EF-lessons/Recreate_On_Model_Change/Data.Test/README.txt
+++ EF-lessons/Recreate_On_Model_Change/Data.Test/README.txt
@@ -1,10 +1,10 @@
 ﻿The models and setup for this project were borrowed from this URL: http://msdn.microsoft.com/en-us/data/jj193542.aspx
 
 To see the database get dropped and re-created, do the following:
-1. Run the project once and verify that the database gets created.
+1. Run the Data.Test.BloggerContextText.CreateDatabase unit test once and verify that the database gets created.
 2. Open Data.Model.Blog.cs and uncomment the Url property
 3. Open the Data.BloggerContextInitializer and uncomment line 18
-4. Open Application.Program and uncomment line 26
-5. Run the project again.
+4. Open Data.Test.BloggerContextText.CreateDatabase and uncomment line 25
+5. Run the unit test again.
 
 --> Notice that the database is dropped and re-created with the new Url column in the Blog table.
