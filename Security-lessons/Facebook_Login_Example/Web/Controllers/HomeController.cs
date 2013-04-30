21040a95ad79af9480e7abe842e4a19ec2b94c97
Stan Butler <sbutler@nerdery.com>
2013-04-30 16:18:29 -0500

    In the middle of updating facebook user pics with an ajax call.

--- Security-lessons/Facebook_Login_Example/Web/Controllers/HomeController.cs
+++ Security-lessons/Facebook_Login_Example/Web/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Web.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-        private const string FACEBOOK_GRAPH_URL = "https://graph.facebook.com/";
+        public const string FACEBOOK_GRAPH_URL = "https://graph.facebook.com/";
 
         public ActionResult Index()
         {
@@ -28,7 +28,7 @@ namespace Web.Controllers
             return View();
         }
 
-        public async Task<ActionResult> MyFriends()
+        public Task<ActionResult> MyFriends()
         {
             //Get a list of friends for the currently logged in user
             var getFriendsUrl = string.Format("{0}/me/friends?access_token={1}", FACEBOOK_GRAPH_URL,
@@ -38,6 +38,15 @@ namespace Web.Controllers
             var friendIds = (from friend in myFriends["data"]
                             select (string)friend["id"]).Take(20).ToList();
 
+            var friendsList = JsonConvert.DeserializeObject<List<FacebookUser>>(json);
+
+            //Get the pic for each user from facebook because they are retarded and don't include a link to that in the user information!
+
+            return View(friendsList);
+        }
+
+        public static List<FacebookUser> GetFacebookFriends(List<string> friendIds)
+        {
             var friendsList = new List<FacebookUser>();
 
             friendIds.ForEach(f =>
@@ -49,9 +58,7 @@ namespace Web.Controllers
                 friendsList.Add(addMe);
             });
 
-            //Get the pic for each user from facebook because they are retarded and don't include a link to that in the user information!
-
-            return View(friendsList);
+            return friendsList;
         }
     }
 }
