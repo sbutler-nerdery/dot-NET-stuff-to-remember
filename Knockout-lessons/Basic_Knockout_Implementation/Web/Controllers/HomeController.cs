17926327b7f3c12c84ea260f037f2e48b3290827
Stan Butler <sbutler@nerdery.com>
2013-05-02 10:04:39 -0500

    Basic knockout binding in place!

---
+++ Knockout-lessons/Basic_Knockout_Implementation/Web/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var blogs = new List<Blog>();
+
+            for (var i = 0; i < 5; i++)
+            {
+                var blog = new Blog {BlogId = i, Name = string.Format("Blog {0}",i),
+                Posts = new List<Post>()};
+                for (var j = 0; j < 2; j++)
+                {
+                    var post = new Post { PostId = j, Title = string.Format("Post #{0}",j), 
+                        Content = string.Format("Meaning full content for post #{0}",j)};
+                    blog.Posts.Add(post);
+                }
+                blogs.Add(blog);
+            }
+
+            ViewBag.Json = JsonConvert.SerializeObject(blogs);
+
+            return View();
+        }
+    }
+}
---
+++ Knockout-lessons/Basic_Knockout_Implementation/Web/Scripts/ViewModel.js
@@ -0,0 +1,36 @@
+﻿var KnockoutBasics = KnockoutBasics || {};
+KnockoutBasics.ViewModels = KnockoutBasics.ViewModels || {};
+
+(function ($, KnockoutBasics) {
+    KnockoutBasics.ViewModels.Blog = function () {
+        var self = this;
+        self.BlogId = ko.observable();
+        self.Name = ko.observable();
+        self.Posts = ko.observableArray();
+        return self;
+    };
+    
+    KnockoutBasics.ViewModels.Post = function () {
+        var self = this;
+        self.PostId = ko.observable();
+        self.Title = ko.observable();
+        self.Content = ko.observable();
+        return self;
+    };
+
+    KnockoutBasics.ViewModels.ContentViewModel = function(blogsJson) {
+        var self = this;
+        self.blogs = ko.observableArray();
+        self.page = ko.observable(0);
+        self.pageSize = ko.observable(10);
+
+        ko.mapping.fromJS(blogsJson, null, self.blogs);
+
+        //Methods
+        self.applyBindings = function(toElement) {
+            ko.applyBindings(self, $(toElement).get(0));
+        };
+
+        return self;
+    };
+}(jQuery, KnockoutBasics));
