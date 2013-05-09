0a6b50d2b10a9e5f3f736a5067b6911dee0aaba9
Stan Butler <sbutler@nerdery.com>
2013-05-09 11:18:58 -0500

    MVC mobile starter is working properly.

--- Mobile-lessons/MVC_Mobile_Starter/MvcMobile/Global.asax.cs
+++ Mobile-lessons/MVC_Mobile_Starter/MvcMobile/Global.asax.cs
@@ -17,11 +17,11 @@ namespace MvcMobile
     {
         protected void Application_Start()
         {
-            DisplayModeProvider.Instance.Modes.Insert(0, new DefaultDisplayMode("iPhone")
-            {
-                ContextCondition = (context => context.GetOverriddenUserAgent().IndexOf
-                    ("iPhone", StringComparison.OrdinalIgnoreCase) >= 0)
-            });
+            //DisplayModeProvider.Instance.Modes.Insert(0, new DefaultDisplayMode("iPhone")
+            //{
+            //    ContextCondition = (context => context.GetOverriddenUserAgent().IndexOf
+            //        ("iPhone", StringComparison.OrdinalIgnoreCase) >= 0)
+            //});
 
             AreaRegistration.RegisterAllAreas();
 
---
+++ Mobile-lessons/MVC_Mobile_Starter/README.txt
@@ -0,0 +1,4 @@
+This project was downloaded from the following URL:
+http://www.asp.net/mvc/tutorials/mvc-4/aspnet-mvc-4-mobile-features
+
+In addition, this site is an excellent tutorial on how to mobile enable your MVC 4 sites!
