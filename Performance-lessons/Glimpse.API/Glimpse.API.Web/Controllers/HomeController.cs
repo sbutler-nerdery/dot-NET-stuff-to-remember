61151dffc1640dd61e40ce27a5fc540f398e13ae
Stan Butler <sbutler@nerdery.com>
2013-08-21 16:40:33 -0500

    Added the EF5 package. Now I have a nice SQL tab in my glimpse! Also... remember to go to glimpse.axd to turn it on.

--- Performance-lessons/Glimpse.API/Glimpse.API.Web/Controllers/HomeController.cs
+++ Performance-lessons/Glimpse.API/Glimpse.API.Web/Controllers/HomeController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Glimpse.API.Web.Data;
+using Glimpse.API.Web.Models;
 
 namespace Glimpse.API.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private IRepository<Person> _PersonRepository;
+ 
+        public HomeController(IRepository<Person> repository)
+        {
+            _PersonRepository = repository;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var people = _PersonRepository.GetAll("Gadgets");
+            return View(people);
         }
     }
 }
