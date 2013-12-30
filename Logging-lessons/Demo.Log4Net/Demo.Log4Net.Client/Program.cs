cffadf4d66c4118b6b11b406f078b8fbabce4150
Stan Butler <sbutler@nerdery.com>
2013-12-30 15:26:09 -0600

    Added log4net demo.

---
+++ Logging-lessons/Demo.Log4Net/Demo.Log4Net.Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.Log4Net.Core;
+
+namespace Demo.Log4Net.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Logging now...");
+
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                LoggingService.GetInstance().LogError(ex, typeof(Program));
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Logging-lessons/Demo.Log4Net/Demo.Log4Net.Core/LoggingService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net.Config;
+
+namespace Demo.Log4Net.Core
+{
+    public class LoggingService
+    {
+        private static LoggingService _instance;
+
+        private LoggingService()
+        {
+            XmlConfigurator.Configure();
+        }
+
+        public static LoggingService GetInstance()
+        {
+            return _instance ?? (_instance = new LoggingService());
+        }
+
+        public void LogError(Exception ex, Type logType)
+        {
+            log4net.ILog eventLogger = log4net.LogManager.GetLogger("EventLogger");
+            log4net.ILog fileLogger = log4net.LogManager.GetLogger("FileLogger");
+            eventLogger.Error("test error", ex);
+            fileLogger.Error("test error", ex);            
+        }
+    }
+}
